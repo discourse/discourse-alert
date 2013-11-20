ef7e95abe0980cf516033cfeb1c486b18bb9d53c
Régis Hanol <regis@hanol.fr>
2013-11-20 18:32:25 +0100

    fix code order for better comprehension

--- assets/javascripts/alert.js
+++ assets/javascripts/alert.js
@@ -1,11 +1,5 @@
 (function() {
-
-  Discourse.TopicFooterButtonsView.reopen({
-    addAlertButton: function() {
-      this.attachViewClass(Discourse.AlertButton);
-    }.on("additionalButtons")
-  });
-
+  
   Discourse.AlertButton = Discourse.ButtonView.extend({
     text: 'alert',
     title: 'display the topic title in an alert',
@@ -17,6 +11,12 @@
     renderIcon: function(buffer) {
       buffer.push("<i class='icon icon-warning-sign'></i>");
     }
+  });
+
+  Discourse.TopicFooterButtonsView.reopen({
+    addAlertButton: function() {
+      this.attachViewClass(Discourse.AlertButton);
+    }.on("additionalButtons")
   });
 
 })();
